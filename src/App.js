6aa7fd3c177a37e4c9c6982cd84ea486900f2648
Nicholas Pung <nckpng95@gmail.com>
2019-11-18 15:43:44 -0800

    Added a way to input API key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">Hi</div>;
+  const [key, setKey] = useState("");
+  const [currentApiKey, setCurrentApiKey] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setCurrentApiKey(JSON.parse(localStorage.getItem("token")));
+    }
+  }, []);
+
+  const onApiKeyChange = event => {
+    setKey(event.target.value);
+  };
+
+  const onApiKeySubmit = event => {
+    event.preventDefault();
+    localStorage.setItem("token", JSON.stringify(key));
+    setCurrentApiKey(key);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={event => onApiKeySubmit(event)}>
+        <label for="token">Input your API Key here!</label>
+        <input
+          name="token"
+          placeholder="Type in your api key"
+          onChange={event => onApiKeyChange(event)}
+          value={key}
+        />
+        <button type="submit">Set Api Key</button>
+      </form>
+      <div>
+        <h4>Current Api Key</h4>
+        <p>{currentApiKey}</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
