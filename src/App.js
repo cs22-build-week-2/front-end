5c266574c024ad4188c2154580df23c45300d159
rushi444 <rushi44@gmail.com>
2019-11-18 22:21:44 -0800

    Added player init hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React, { useState, useEffect } from "react";
-import { initialize, move } from "./endpointCalls";
+import React, { useState, useEffect } from 'react';
+import { initialize, move } from './endpointCalls';
 
 function App() {
-  const [key, setKey] = useState("");
-  const [currentApiKey, setCurrentApiKey] = useState("");
-  const [playerStatus, setPlayerStatus] = useState({});
+  const playerState = {
+    name: '',
+    cooldown: 0,
+    encumbrance: 0,
+    strength: 0,
+    speed: 0,
+    gold: 0,
+    bodywear: '',
+    footwear: '',
+    inventory: [],
+    status: [],
+    errors: [],
+    messages: [],
+  };
+
+  const [key, setKey] = useState('');
+  const [currentApiKey, setCurrentApiKey] = useState('');
+  const [playerStatus, setPlayerStatus] = useState(playerState);
   const [cooldown, setCooldown] = useState(0);
 
   useEffect(() => {
     initialize()
       .then(res => {
-        setPlayerStatus(res.data)
-        setCooldown(res.data.cooldown)
+        let playerName = res.data.name;
+        let cooldown = res.data.cooldown;
+        let encumbrance = res.data.encumbrance;
+        let strength = res.data.strength;
+        let speed = res.data.speed;
+        let gold = res.data.gold;
+        let bodywear = res.data.bodywear;
+        let inventory = res.data.inventory;
+        let status = res.data.status;
+        let errors = res.data.errors;
+        let messages = res.data.messages;
+
+        setPlayerStatus({
+          name: playerName,
+          cooldown: cooldown,
+          encumbrance: encumbrance,
+          strength: strength,
+          speed: speed,
+          gold: gold,
+          bodywear: bodywear,
+          footwear: footwear,
+          inventory: inventory,
+          status: status,
+          errors: errors,
+          messages: messages,
+        });
       })
       .catch(err => console.log(err));
   }, []);
 
   useEffect(() => {
-    if (localStorage.getItem("token")) {
-      setCurrentApiKey(localStorage.getItem("token"));
+    if (localStorage.getItem('token')) {
+      setCurrentApiKey(localStorage.getItem('token'));
     }
   }, []);
 
@@ -28,7 +67,7 @@ function App() {
         setCooldown(cooldown - 1);
       }
     }, 1000);
-    return () => clearTimeout(timer)
+    return () => clearTimeout(timer);
   }, [cooldown]);
 
   const onApiKeyChange = event => {
@@ -37,30 +76,30 @@ function App() {
 
   const onApiKeySubmit = event => {
     event.preventDefault();
-    localStorage.setItem("token", key);
+    localStorage.setItem('token', key);
     setCurrentApiKey(key);
   };
 
   const onMoveButton = direction => {
     move({ direction })
       .then(res => {
-        setPlayerStatus(res.data)
-        setCooldown(res.data.cooldown)
+        setPlayerStatus(res.data);
+        setCooldown(res.data.cooldown);
       })
       .catch(err => console.log(err));
   };
 
   return (
-    <div className="App">
+    <div className='App'>
       <form onSubmit={event => onApiKeySubmit(event)}>
-        <label htmlFor="token">Input your API Key here!</label>
+        <label htmlFor='token'>Input your API Key here!</label>
         <input
-          name="token"
-          placeholder="Type in your api key"
+          name='token'
+          placeholder='Type in your api key'
           onChange={event => onApiKeyChange(event)}
           value={key}
         />
-        <button type="submit">Set Api Key</button>
+        <button type='submit'>Set Api Key</button>
       </form>
       <div>
         <h4>Current Api Key</h4>
@@ -78,31 +117,27 @@ function App() {
       <div>
         <h3>Move Buttoms</h3>
         <button
-          type="button"
-          onClick={() => onMoveButton("n")}
-          disabled={cooldown}
-        >
+          type='button'
+          onClick={() => onMoveButton('n')}
+          disabled={cooldown}>
           Up
         </button>
         <button
-          type="button"
-          onClick={() => onMoveButton("s")}
-          disabled={cooldown}
-        >
+          type='button'
+          onClick={() => onMoveButton('s')}
+          disabled={cooldown}>
           Down
         </button>
         <button
-          type="button"
-          onClick={() => onMoveButton("w")}
-          disabled={cooldown}
-        >
+          type='button'
+          onClick={() => onMoveButton('w')}
+          disabled={cooldown}>
           Left
         </button>
         <button
-          type="button"
-          onClick={() => onMoveButton("e")}
-          disabled={cooldown}
-        >
+          type='button'
+          onClick={() => onMoveButton('e')}
+          disabled={cooldown}>
           Right
         </button>
         <h5>Move Cooldown</h5>
